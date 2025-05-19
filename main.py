2947e70ad7635e4897037fc17fa623764fca0634
Juan Frois <juan.frois9@gmail.com>
2025-05-18 23:07:37 -0300

    Ajuste no arquivo main principal, para inclusão da nova atividade: Alimentacao.

--- main.py
+++ main.py
@@ -3,33 +3,51 @@ from Calculadora import calculadoraMain
 from CalculoBaskara import baskaraMain
 from EscolhaRoupa import escolhaRoupa
 from Patinete import calculo_velocidadeMedia
+from Alimentacao import alimentacaoMain
+
+mensagemInicial = "Olá, seja bem-vindo ao nosso programa!"
+mensagemFinal = "Obrigado por usar nosso programa!"
+mensagemErro = "Desculpe, não entendi sua resposta. Tente novamente."
 
 def main():
-    print('Escolha uma opção:')
-    print('1 - Ajudando doutor')
-    print('2 - Calculadora')
-    print('3 - Calculo baskara')
-    print('4 - Escolha roupa')
-    print('5 - Patinete')
+    print(mensagemInicial)
+    print('----------------------------------')
+    opcao = 0
     
-    try:
-        opcao = int(input('Opção: '))
-    except ValueError:
-        print('Por favor, insira um número válido.')
-        return
+    while opcao != 7:
+        print('Escolha uma opção:')
+        print('1 - Ajudando doutor')
+        print('2 - Calculadora')
+        print('3 - Calculo baskara')
+        print('4 - Escolha roupa')
+        print('5 - Patinete')
+        print('6 - Alimentação')
+        print('7 - Sair')
+        
+        try:
+            opcao = int(input('Digite sua opção: '))
+            if opcao < 1 or opcao > 7:
+                raise ValueError
+        except ValueError:
+            print(mensagemErro)
+            print('----------------------------------')
 
-    if opcao == 1:
-        doutorMain()
-    elif opcao == 2:
-        calculadoraMain()
-    elif opcao == 3:
-        baskaraMain()
-    elif opcao == 4:
-        escolhaRoupa()
-    elif opcao == 5:
-        calculo_velocidadeMedia()
-    else:
-        print('Opção inválida!')
-
+        if opcao == 1:
+            doutorMain()
+        elif opcao == 2:
+            calculadoraMain()
+        elif opcao == 3:
+            baskaraMain()
+        elif opcao == 4:
+            escolhaRoupa()
+        elif opcao == 5:
+            calculo_velocidadeMedia()
+        elif opcao == 6:
+            alimentacaoMain()
+        else:
+            print(mensagemFinal)
+            print('----------------------------------')
+            break
+        
 if __name__ == '__main__':
     main()
