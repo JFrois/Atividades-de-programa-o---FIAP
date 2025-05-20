ebf21529311c2ac7387866e331de263d7e28cd84
Juan Frois <juan.frois9@gmail.com>
2025-05-19 21:16:01 -0300

    Retiramos os caches existentes.

--- OperadoresLogicos/AjudandoDoutor/doutorMain.py
+++ OperadoresLogicos/AjudandoDoutor/doutorMain.py
@@ -1,7 +1,9 @@
-
 def main():
-    'O doutor Henry Jones Junior estabeleceu uma regra com seus alunos da disciplina de Arqueologia: todos os que obtiverem nota maior do que 8,5 na sua prova semestral serão convidados para uma visita de campo na América do Sul.'
-    'Nosso programa deve solicitar o e-mail e a nota do aluno, exibindo a mensagem “ENVIANDO CONVITE” caso a nota do aluno satisfaça a condição proposta.Utilizando apenas um if simples, podemos resolver esse problema rapidamente! Basta solicitarmos a digitação dos dados, converter a nota para números reais (float) e verificar se ela atende à condição do professor Jones.'
+    """
+    O doutor Henry Jones Junior estabeleceu uma regra com seus alunos da disciplina de Arqueologia:
+    todos os que obtiverem nota maior do que 8,5 na prova semestral serão convidados para uma visita de campo na América do Sul.
+    O programa solicita RM, e-mail e nota do aluno, e imprime "ENVIANDO CONVITE" se a nota for maior que 8.5.
+    """
 
     rm = input('Digite o RM do aluno: ')
     email = input('Digite o e-mail do aluno: ')
